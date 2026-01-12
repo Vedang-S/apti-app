14fc6190e70d0560f8d1c5e4056abfd0ff0f6a6e
Vedang-S <173562448+Vedang-S@users.noreply.github.com>
2026-01-12 11:56:40 +0000

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return <>
+    <div className="application-body">
+        <h1 className="title">aptitude learning & practice application</h1>
+    </div>
+  </>;
+}
+
+export default App;
