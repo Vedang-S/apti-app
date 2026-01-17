ce73624a93a71365473a9a28da8d40bc57988124
Vedang-S <173562448+Vedang-S@users.noreply.github.com>
2026-01-17 18:17:37 +0000

    Update backend path

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@ import { app } from "./src/app.js";
 
 let PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`SERVER IS LISTENING ON PORT ${PORT}`);
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
-  res.send("SERVER IS RUNNING");
+  res.status(200).send("SERVER IS RUNNING");
 });
 
 export { app };
